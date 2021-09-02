0d8a261d98149fa0518cbf10a388dc308909054e
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-09-02 11:51:43 +0200

    form insert

--- components/styles.js
+++ components/styles.js
@@ -1,58 +1,117 @@
-import styled from "styled-components";
-import {View, Text, Image} from 'react-native';
-import Constants  from "expo-constants";
+import styled from 'styled-components';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
 
 const StatusBarHeight = Constants.statusBarHeight;
 
 //cores
 export const Colors = {
-    primary: "#ffffff",
-    secondary: "#E5E7EB",
-    tertiary: "#1F2937",
-    darkLight: "#9CA3AF",
-    brand: "#6D28D9",
-    green: "#10B981",
-    red: "#EF4444",
+  primary: '#ffffff',
+  secondary: '#E5E7EB',
+  tertiary: '#1F2937',
+  darkLight: '#9CA3AF',
+  brand: '#6D28D9',
+  green: '#10B981',
+  red: '#EF4444',
 };
 
-const {primary, secondary, tertiary, darkLight, brand, green, red} = Colors;
+const { primary, secondary, tertiary, darkLight, brand, green, red } = Colors;
 
 export const StyledContainer = styled.View`
-    flex: 1;
-    padding: 25px;
-    padding-top: ${StatusBarHeight + 10};
-    background-color: ${primary};
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 10};
+  background-color: ${primary};
 `;
 
 export const InnerContainer = styled.View`
-    flex: 1;
-    width: 100%;
-    align-items: center;
-    padding-top: 50px; 
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  padding-top: 50px;
 `;
 
 export const PageLogo = styled.Image`
-   
-    width: 150px;
-    height: 100px;
+  width: 150px;
+  height: 100px;
 `;
 
 export const PageTitle = styled.Text`
-    font-size: 30px;
-    text-align: center;
-    font-weight: bold;
-    color: ${brand};
-    padding: 10px;
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${brand};
+  padding: 10px;
 `;
 
 export const SubTitle = styled.Text`
-    font-size: 18px;
-    margin-bottom: 20px;
-    letter-spacing: 1px;
-    font-weight: bold;
-    color: ${tertiary}; 
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${tertiary};
 `;
 
 export const StyledFormArea = styled.View`
-    width: 90%;
+  width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${secondary};
+  padding: 16px;
+  padding-left: 50px;
+  padding-right: 50px;
+  border-radius: 3px;
+  font-size: 16px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+  color: ${tertiary};
+  font-size: 13px;
+  text-align: left;
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  right: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${brand};
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  margin-vertical: 5px;
+  height: 60px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${primary};
+  font-size: 18px;
+`;
+
+export const MsgBox = styled.Text`
+  text-align: center;
+  font-size: 16px;
+`;
+
+export const Line = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${darkLight};
+    margin-vertical: 10px;
 `;
--- screens/Login.js
+++ screens/Login.js
@@ -1,40 +1,108 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 
 //formik
 import { Formik } from 'formik';
 
+//icones
+import { Octicons, Ionicons } from '@expo/vector-icons';
+
+//colors
+import { Colors } from './../components/styles';
+
 import {
-    StyledContainer,
-    InnerContainer,
-    PageLogo,
-    PageTitle,
-    SubTitle,
-    StyledFormArea
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  RightIcon,
+  StyledButton,
+  ButtonText,
+  MsgBox,
+  Line
 } from './../components/styles';
 
+import { View } from 'react-native';
 
+//cores
+const { brand, darkLight } = Colors;
 
 const Login = () => {
-    return (
-        <StyledContainer>
-            <StatusBar style="dark" />
-            <InnerContainer>
-                <PageLogo resizeMode="cover" source={require('../assets/logoo.png')} />
-                <PageTitle> Whatsapp 1 </PageTitle>
-                <SubTitle> Preencha os campos </SubTitle>
+    const [hidePassword, setHidePassword] = useState(true);
 
-                <Formik
-                    initialValues={{email: '', password: ''}}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                >
-                    
-                </Formik>
-            </InnerContainer>
-        </StyledContainer>
-    );
-}
+  return (
+    <StyledContainer>
+      <StatusBar style="dark" />
+      <InnerContainer>
+        <PageLogo resizeMode="cover" source={require('../assets/logoo.png')} />
+        <PageTitle> Bem Vindo </PageTitle>
+        <SubTitle> Login da conta </SubTitle>
+
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <StyledFormArea>
+              <MyTextInput
+                label="Email"
+                icon="mail"
+                placeholder="qualquer@gmail.com"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                keyboardType="email-address"
+              />
+              <MyTextInput
+                label="Senha"
+                icon="lock"
+                placeholder="* * * * * *"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                secureTextEntry={hidePassword}
+                isPassword={true}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+              />
+              <MsgBox>...</MsgBox>
+              <StyledButton onPress={handleSubmit}>
+                  <ButtonText>Entrar</ButtonText>
+              </StyledButton>
+              <Line />
+            </StyledFormArea>
+            
+          )}
+        </Formik>
+      </InnerContainer>
+    </StyledContainer>
+  );
+};
+
+const MyTextInput = ({ label, icon,isPassword, hidePassword, setHidePassword,...props }) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+          <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+             <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight}/> 
+          </RightIcon>
+      )}
+    </View>
+  );
+};
 
 export default Login;
