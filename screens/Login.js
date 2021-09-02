415e6d94f904ccc61c876843c5f4d5990ff1f87b
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-09-02 10:08:06 +0200

    form insert

--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
+//formik
+import { Formik } from 'formik';
+
 import {
     StyledContainer,
     InnerContainer,
     PageLogo,
-    PageTitle
+    PageTitle,
+    SubTitle,
+    StyledFormArea
 } from './../components/styles';
+
+
 
 const Login = () => {
     return (
         <StyledContainer>
+            <StatusBar style="dark" />
             <InnerContainer>
                 <PageLogo resizeMode="cover" source={require('../assets/logoo.png')} />
-                <PageTitle>Login</PageTitle>
+                <PageTitle> Whatsapp 1 </PageTitle>
+                <SubTitle> Preencha os campos </SubTitle>
+
+                <Formik
+                    initialValues={{email: '', password: ''}}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    
+                </Formik>
             </InnerContainer>
         </StyledContainer>
     );
