0119baa25938c46b1df25c142f970e3afe9355a7
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-10-22 15:58:26 +0200

    formulario cadastro/welcome

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,50 +1,26 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-//formik
-import { Formik } from 'formik';
-
-//icones
-import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
-
 //colors
-import { Avatar, Colors, WelcomeContainer, WelcomeImage } from './../components/styles';
+import { Avatar, WelcomeContainer, WelcomeImage } from './../components/styles';
 
 import {
-  StyledContainer,
   InnerContainer,
-  PageLogo,
   PageTitle,
   SubTitle,
   StyledFormArea,
-  LeftIcon,
-  StyledInputLabel,
-  StyledTextInput,
-  RightIcon,
   StyledButton,
   ButtonText,
-  MsgBox,
   Line,
-  StyledButtong,
-  ExtraView,
-  ExtraText,
-  TextLink,
-  TextLinkContent,
 } from './../components/styles';
 
-import { View } from 'react-native';
-
-//cores
-const { brand, darkLight, primary } = Colors;
-
 const Welcome = () => {
-  const [hidePassword, setHidePassword] = useState(true);
 
   return (
     <>
       <StatusBar style="dark" />
       <InnerContainer>
-      <WelcomeImage resizeMode="cover" source={require('../assets/logoo.png')} />
+      <WelcomeImage resizeMode="contain" source={require('../assets/Frutas.jpg')} />
         <WelcomeContainer>
           <PageTitle welcome={true}> Welcome! Brow </PageTitle>
           <SubTitle welcome={true}> Iyonissio Daniel Sitoe </SubTitle>
@@ -59,23 +35,6 @@ const Welcome = () => {
         </WelcomeContainer>
       </InnerContainer>
     </>
-  );
-};
-
-const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
-  return (
-    <View>
-      <LeftIcon>
-        <Octicons name={icon} size={30} color={brand} />
-      </LeftIcon>
-      <StyledInputLabel>{label}</StyledInputLabel>
-      <StyledTextInput {...props} />
-      {isPassword && (
-        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
-          <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
-        </RightIcon>
-      )}
-    </View>
   );
 };
 
